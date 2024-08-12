5ff6cc62a1ccf4a2120558fc5fdd84026e018482
Andrew Duffy <andreweduffy@gmail.com>
2024-08-12 17:53:54 -0400

    README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# fsst
+# fsst-rs
+
+A pure-Rust, zero-dependency implementation of the [FSST string compression algorithm][whitepaper].
+
+FSST is a string compression algorithm meant for use in database systems. It was designed by
+[Peter Boncz, Thomas Neumann, and Viktor Leis][whitepaper]. It provides 1-3GB/sec compression
+and decompression of strings at compression rates competitive with or better than LZ4.
+
+**NOTE**: This  current implementation is still in-progress, please use at your own risk.
+
+
+[whitepaper]: https://www.vldb.org/pvldb/vol13/p2649-boncz.pdf
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
-//! A pure-Rust, zero-dependency implementation of the [FSST string compression algorithm][whitepaper].
-//!
-//! FSST is a string compression algorithm meant for use in database systems. It was designed by
-//! [Peter Boncz, Thomas Neumann, and Viktor Leis][whitepaper]. It provides 1-3GB/sec compression
-//! and decompression of strings at compression rates competitive with or better than LZ4.
-//!
-//! NOTE: This  current implementation is still in-progress, please use at your own risk.
-//!
-//! [whitepaper]: https://www.vldb.org/pvldb/vol13/p2649-boncz.pdf
-
+#![doc = include_str!("../README.md")]
 use std::fmt::{Debug, Formatter};
 
 pub use builder::*;
