50ff02ec67e484eff9d67536daeffc9123d45aad
Nicholas Gates <nick@nickgates.com>
2025-03-12 13:38:43 +0000

    8-byte unrolling

--- src/lib.rs
+++ src/lib.rs
@@ -325,7 +325,7 @@ impl<'a> Decompressor<'a> {
                     let next_block = in_ptr.cast::<u64>().read_unaligned();
                     let escape_mask = (next_block & 0x8080808080808080)
                         & ((((!next_block) & 0x7F7F7F7F7F7F7F7F) + 0x7F7F7F7F7F7F7F7F)
-                        ^ 0x8080808080808080);
+                            ^ 0x8080808080808080);
 
                     // If there are no escape codes, we write each symbol one by one.
                     if escape_mask == 0 {
