302e7cba8f0bc22e2924d98a4a6388667cb731c1
Robert Kruszewski <github@robertk.io>
2026-06-24 12:54:31 +0100

    format
    Signed-off-by: Robert Kruszewski <github@robertk.io>

--- src/lib.rs
+++ src/lib.rs
@@ -999,7 +999,8 @@ mod test {
             builder.insert(symbol, *len as usize);
         }
         let compressor = builder.build();
-        let built_symbols: &[u64] = unsafe { mem::transmute(&compressor.symbol_table()[0..compressor.n_symbols()]) };
+        let built_symbols: &[u64] =
+            unsafe { mem::transmute(&compressor.symbol_table()[0..compressor.n_symbols()]) };
         assert_eq!(built_symbols, symbols);
     }
 }
